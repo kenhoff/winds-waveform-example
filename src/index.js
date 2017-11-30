5831c418999a8c3e502136c447dc8b170eb0eb54
Ken Hoff <ken@hoff.tech>
2017-11-29 17:40:31 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Player from "./Player.jsx";
+
+ReactDOM.render(<Player file="/girl-from-ipanema.mp3"/>, document.getElementById("root"));
