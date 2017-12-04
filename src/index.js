66b10e879605126916b1f6a0e3d64c307ff1bbd4
Ken Hoff <ken@hoff.tech>
2017-12-04 12:49:31 -0700

    ripping out everything but the basics

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Player from "./Player.jsx";
-
-ReactDOM.render(<Player file="/girl-from-ipanema.mp3"/>, document.getElementById("root"));
